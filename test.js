f09ac7e0f60e41c098e8d967c97c14c663f42342
dellclm1100 <275298711@qq.com>
2019-06-04 14:28:37 +0800

    更新甘特图

---
+++ test.js
@@ -0,0 +1,40 @@
+var result = [
+    {
+        code:200,
+        msg:"请求成功",
+        data:"假数据"
+    },
+    {
+        code:999,
+        msg:"token过期失效"
+    }
+]
+
+var axios = {
+    request(token){
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                let res = result[Math.round(Math.random())]
+                resolve(res)
+            },1000)
+        })
+    }
+}
+function request(token) {
+
+    return axios.request(token).then(response=>{
+        if(response.code==200){
+            return response
+        }else{
+            console.log("刷新token");
+            return new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    resolve(request(true))
+                },1000);
+            })
+        }
+    }).catch(error => console.log(error));
+}
+request().then((res)=>{
+    console.log(res);
+});
